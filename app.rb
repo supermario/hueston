e96a16874ab6ff69ea9c0c500700427490b51a7f
Mario Rogic <hello@mario.net.au>
2014-08-23 22:25:30 +1000

    Single execution now sets colour time correctly

--- app.rb
+++ app.rb
@@ -4,23 +4,22 @@ require 'color'
 client = LIFX::Client.lan
 client.discover! do |c|
   c.lights.with_label('Hour')
+  c.lights.with_label('Minute')
 end
 
 colours = ['f04950','f58d4e','f9a54b','fece3e','fff533','a3fd39','33b8a5','33a7d8','3276b5','8869ad','b868ad','e966ac']
 
-light = client.lights.with_label('Hour')
+hour_colour = colours[Time.now.hour % 12]
+minute_colour = colours[Time.now.minute / 5]
 
-colours.each do |hex|
+light_set_hex 'Hour' hour_colour
+light_set_hex 'Minute' minute_colour
 
+def light_set_hex light_name hex
+  light = client.lights.with_label(light_name)
   c = Color::RGB.by_hex(hex).to_hsl
-  # p c.hue
-  # p c.saturation
-  # p c.lightness
-
   set = LIFX::Color.hsl(c.hue, c.saturation / 100, c.luminosity / 1300)
   light.set_color(set)
-
-  sleep 2
 end
 
 client.flush
